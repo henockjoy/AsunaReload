e135912c9077ba7f886aeccaba4c82fc9859a9a9
Henock Joy <89825644+henockjoy@users.noreply.github.com>
2025-08-24 14:29:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /AsunaReload
 fi
-cd /shobanafilterbot
+cd /AsunaReload
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
